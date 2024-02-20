2fd90d1f40d1504fe6ed5adb77ed3b8bc3d1ee9f
Akbaralievva <alinaakbaralieva0@gmail.com>
2024-02-20 08:46:59 +0600

    Initial Commit

---
+++ Home_4month_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home_4month_3
+//
+//  Created by A LINA on 20/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
